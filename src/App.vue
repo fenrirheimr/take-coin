a4b57decd36e54946dc39da1652161b67b138b4f
Glushkov Vladimir <fenrirheimr@gmail.com>
2024-03-22 02:25:10 +0500

    fix container height

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ watch(
 
 <style scoped lang="scss">
 main {
-  @include flex(column, center, center);
+  @include flex(column, center, flex-start);
   position: relative;
   background: #010201 url('data:image/svg+xml,<svg width="393" height="843" viewBox="0 0 393 843" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(%23filter0_f_674_33)"><ellipse cx="196.192" cy="807" rx="658" ry="479" fill="url(%23paint0_radial_674_33)"/></g><defs><filter id="filter0_f_674_33" x="-789.208" y="0.600006" width="1970.8" height="1612.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="163.7" result="effect1_foregroundBlur_674_33"/></filter><radialGradient id="paint0_radial_674_33" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(196.192 807) rotate(90) scale(479 658)"><stop stop-color="%2328C629"/><stop offset="1" stop-color="%23BDFF00"/></radialGradient></defs></svg>') repeat-x fixed bottom;
   min-width: 393px; // TODO: убрать после
