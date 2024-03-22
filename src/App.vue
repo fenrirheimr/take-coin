7d2d94e9d401fe3345d66ce366a5ef751eb74726
Glushkov Vladimir <fenrirheimr@gmail.com>
2024-03-22 17:34:11 +0500

    fix backgound gradient

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@ defineProps({
 const tg = window.Telegram.WebApp
 
 passportStore().userAuth()
-// const id = '286133104'
-// passportStore().setTgUserId(id)
-passportStore().setTgUserId(tg?.initDataUnsafe?.user?.id)
+const id = '286133104'
+passportStore().setTgUserId(id)
+// passportStore().setTgUserId(tg?.initDataUnsafe?.user?.id)
 onMounted(async () => {
   // await passportStore().userAuth()
   // const id = '286133104'
@@ -46,16 +46,15 @@ watch(
 main {
   @include flex(column, center, flex-start);
   position: relative;
-  background: #010201;
   //background: #010201 radial-gradient(50% 50% at 50% 50%, #28C629 0%, #BDFF00 100%);
-  //background: #010201 url('data:image/svg+xml,<svg width="393" height="843" viewBox="0 0 393 843" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(%23filter0_f_674_33)"><ellipse cx="196.192" cy="807" rx="658" ry="479" fill="url(%23paint0_radial_674_33)"/></g><defs><filter id="filter0_f_674_33" x="-789.208" y="0.600006" width="1970.8" height="1612.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="163.7" result="effect1_foregroundBlur_674_33"/></filter><radialGradient id="paint0_radial_674_33" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(196.192 807) rotate(90) scale(479 658)"><stop stop-color="%2328C629"/><stop offset="1" stop-color="%23BDFF00"/></radialGradient></defs></svg>') repeat-x fixed bottom;
+  background: #010201 url('data:image/svg+xml,<svg width="393" height="843" viewBox="0 0 393 843" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(%23filter0_f_674_33)"><ellipse cx="196.192" cy="807" rx="658" ry="479" fill="url(%23paint0_radial_674_33)"/></g><defs><filter id="filter0_f_674_33" x="-789.208" y="0.600006" width="1970.8" height="1612.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="163.7" result="effect1_foregroundBlur_674_33"/></filter><radialGradient id="paint0_radial_674_33" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(196.192 807) rotate(90) scale(479 658)"><stop stop-color="%2328C629"/><stop offset="1" stop-color="%23BDFF00"/></radialGradient></defs></svg>') repeat-x fixed bottom;
   width: 100vw;
   min-height: 100vh;
   height: 100vh;
   padding-top: 0;
   z-index: 1;
   &:before {
-    display: block;
+    display: none;
     content: ' ';
     width: 4000vw;
     height: 200vh;
