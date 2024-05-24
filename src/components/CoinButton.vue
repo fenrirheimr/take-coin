8aec94d92ab92d38d14190c9c6a181bc80d88800
Vladimir Glushkov <vladimir.g@bnovo.ru>
2024-05-24 15:21:11 +0500

    change animation speed

--- src/components/CoinButton.vue
+++ src/components/CoinButton.vue
@@ -227,7 +227,7 @@ onMounted(() => {
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  animation: slide-in-fwd-center .6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+  animation: slide-in-fwd-center 1.2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
 @keyframes slide-in-fwd-center {
   0% {
